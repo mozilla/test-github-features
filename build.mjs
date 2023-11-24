e094aeff449032534f11265461e2d1fb5cc0c5e9
Kevin Meinhardt <kmeinhardt7@gmail.com>
2023-11-24 15:28:44 +0000

    chore(): rename delay to de_lay

--- build.mjs
+++ build.mjs
@@ -1,18 +1,18 @@
 #!/usr/bin/env node
 
-const delay = parseInt(process.env.CI_DELAY || 3_000, 10);
+const de_lay = parseInt(process.env.CI_DELAY || 3_000, 10);
 const shouldFail = Boolean(process.env.CI_FAIL || '');
 
-console.log('config', {delay, shouldFail, raw: { delay: process.env.CI_DELAY, shouldFail: process.env.CI_FAIL}});
+console.log('config', {delay: de_lay, shouldFail, raw: { delay: process.env.CI_DELAY, shouldFail: process.env.CI_FAIL}});
 
 await new Promise((resolve, reject) => {
-    console.log(`Running build... for ${delay / 1000} seconds`);
+    console.log(`Running build... for ${de_lay / 1000} seconds`);
     setTimeout(() => {
         if (shouldFail) {
             return reject('failing.... booo');
         }
         resolve(true);
-    }, delay);
+    }, de_lay);
 });
 
 console.log("Built!");
